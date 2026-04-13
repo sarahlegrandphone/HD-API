e4763fe91ab380c75f70f7b7e297b5436cd316f4
sarahlegrandphone <sarah.legrand.phone@gmail.com>
2026-04-13 22:12:23 +0200

    Update SVG path data for image generation

--- api/generate-image.js
+++ api/generate-image.js
@@ -9,8 +9,8 @@ export default function handler(req, res) {
 <path
   id="HEAD"
   d="
-    M186 12
-    A14 14 0 0 1 214 12
+    M190 10
+    A10 10 0 0 1 210 10
 
     L248 88
     A6 6 0 0 1 242 96
