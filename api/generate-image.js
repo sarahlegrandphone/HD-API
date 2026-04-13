8f5949983aa047acdecd7b2347b12b555c1b8405
sarahlegrandphone <sarah.legrand.phone@gmail.com>
2026-04-13 18:57:15 +0200

    Update generate-image.js

--- api/generate-image.js
+++ api/generate-image.js
@@ -77,7 +77,7 @@ export default function handler(req, res) {
 <polygon
   points="50,10 90,90 10,90"
   fill="none"
-  stroke="#2ecc71"
+  stroke="black"
   stroke-width="10"
   stroke-linejoin="round"
 />
